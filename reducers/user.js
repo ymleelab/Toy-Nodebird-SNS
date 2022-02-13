3cacef4d33c5d3cc0cc6d54b2a50a5a5cff19350
ymleelab <ymleelab@gmail.com>
2022-02-13 11:07:46 +0900

    Add Section3 Redux-saga 연동하기 -1 (redux-thunk)

--- reducers/user.js
+++ reducers/user.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const initialState = {
     isLoggedIn: false,
     me: null,
@@ -5,16 +7,63 @@ export const initialState = {
     loginData: {}
 }
 
-//action creater
+/*export const loginAction = (data) => {
+    return (dispatch, getState) => {
+        const state = getState();
+        setTimeout(() => {
+            dispatch(loginRequestAction());
+        }, 2000);
+        axios.get('api/login')
+            .then((res) => {
+                dispatch(loginSuccessAction(res));
+            })
+            .catch((err) => {
+                dispatch(loginFailureAction(err));
+            })
+    }
+}*/
 export const loginAction = (data) => {
     return {
         type: 'LOG_IN',
         data,
     }
 };
+export const loginRequestAction = (data) => {
+    return {
+        type: 'LOG_IN_REQUEST',
+        data,
+    }
+};
+export const loginSuccessAction = (data) => {
+    return {
+        type: 'LOG_IN_SUCCESS',
+        data,
+    }
+};
+export const loginFailureAction = (data) => {
+    return {
+        type: 'LOG_IN_FAILURE',
+        data,
+    }
+};
 export const logoutAction = () => {
     return {
         type: 'LOG_OUT',
+    }
+};
+export const logoutRequestAction = () => {
+    return {
+        type: 'LOG_OUT_REQUEST',
+    }
+};
+export const logoutSuccessAction = () => {
+    return {
+        type: 'LOG_OUT_SUCCESS',
+    }
+};
+export const logoutFailureAction = () => {
+    return {
+        type: 'LOG_OUT_FAILURE',
     }
 };
 
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,11 +1,17 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
 
 import reducer from '../reducers';
 
+const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
+	console.log(action);
+	return next(action);
+};
+
 const configureStore = () => {
-	const middlewares = [];
+	const middlewares = [thunkMiddleware, loggerMiddleware];
 	const enhancer = process.env.NODE_ENV === 'production'
 	? compose(applyMiddleware(...middlewares))
 	: composeWithDevTools(applyMiddleware(...middlewares))
